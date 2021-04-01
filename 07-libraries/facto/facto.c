77c9d5329fc1cf1543578601122af8000c2d1add
Larissa Porto <larissaporto@live.com>
2021-04-01 19:38:43 -0300

    Add library example

---
+++ 07-libraries/facto/facto.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int factorial(int value){
+
+    int fact = 1;
+
+    for (int i = value; i > 0; i--)
+    {
+        // printf("i: %i, value: %i\n", i, fact);
+        fact *= i;
+    }
+    
+    return fact;
+}
+
+
+void outputFactorial(int input){
+
+    printf("Result: %i\n", factorial(input));
+
+}
+
---
+++ 07-libraries/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int factorial(int num);
+void outputFactorial(int num);
+
+int main(){
+
+    outputFactorial(5);
+    // printf("%i",  factorial(5);
+
+    return 0;
+}
